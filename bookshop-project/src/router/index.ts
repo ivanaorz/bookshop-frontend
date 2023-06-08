9e9ef650e59d07a2c8dea04afa1211e72cfe31ec
ivanaorz <yhmu22.orziv@folkuniversitetet.nu>
2023-06-08 21:32:24 +0200

    creatingPaths

--- bookshop-project/src/router/index.ts
+++ bookshop-project/src/router/index.ts
@@ -1,22 +1,57 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import AdminBooksEditView from '@/views/AdminBooksEditView.vue';
+import AdminBooksView from '@/views/AdminBooksView.vue';
+import AdminUsersEditView from '@/views/AdminUsersEditView.vue';
+import AdminUsersView from '@/views/AdminUsersView.vue';
+import GuestView from '@/views/GuestView.vue';
+import LoginView from '@/views/LoginView.vue';
+import RegisterView from '@/views/RegisterView.vue';
+import UserView from '@/views/UserView.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+       },
+       {
+      path: '/guest',
+      name: 'guest',
+      component: GuestView
+       },
+       {
+      path: '/user',
+      name: 'user',
+      component: UserView
+       },
+       {
+      path: '/admin/books',
+      name: 'adminBooks',
+      component: AdminBooksView
+       },
+       {
+      path: '/admin/books/edit',
+      name: 'adminBooksEdit',
+      component: AdminBooksEditView
+       },
+       {
+      path: '/admin/users',
+      name: 'adminUsers',
+      component: AdminUsersView
+       },
+       {
+      path: '/admin/users/edit',
+      name: 'adminUsersEdit',
+      component: AdminUsersEditView
+      }
   ]
 })
 
