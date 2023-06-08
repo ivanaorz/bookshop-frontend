0c93c63b66425af3ff1aa575558f4de4236f4da1
ivanaorz <yhmu22.orziv@folkuniversitetet.nu>
2023-06-08 20:47:53 +0200

    implementingHeaderView

---
+++ bookshop-project/src/components/HeaderView.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1 class="header-name"> {{ title }}</h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {},
+});
+</script>
+
+<Style scoped>
+header {
+  background-color: rgb(116, 200, 220);
+  text-align: center;
+  padding: 1.5em;
+  margin-bottom: 1em;
+}
+</Style>
